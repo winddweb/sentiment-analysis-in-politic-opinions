6f2d928d7afd23f89738f089343ecafc72f0907c
Jialong Li <jialong.li@citrix.com>
2016-04-27 12:18:14 -0700

    Add get stream tweets source code

---
+++ jialong/GetStreamTweets.py
@@ -0,0 +1,46 @@
+#!/usr/local/bin/python3
+import tweepy
+import json
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+
+# twitter application credentials
+consumer_key = 'WKtSw7GMKsY2svfSUsMAPHxeX'
+consumer_secret = 'WQCjZBoiQijGqFEHSZP3u7lzC5tDUYs5BRJpnS8sn8z91g3oan'
+access_token = '713507723325808641-inJY6PB7ZAaRvcg4XEXgvcIkiq9yVmN'
+access_token_secret = 'p7ilE2q6vPuz47kNvodLhGPIFirBm1PFbK6Juud0oGVEn'
+
+tweets = []
+
+class listener(StreamListener):
+
+    def __init__(self):
+        super(listener, self).__init__()
+        self.num_tweets = 0
+
+    def on_data(self, data):
+        all_data = json.loads(data)
+        tweet = all_data["text"]
+        if tweet != '':
+            self.num_tweets += 1
+            #print(tweet)
+            if self.num_tweets <= int(num):
+                tweets.append(tweet)
+                return True
+            else:
+                return False
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+num = str(input("Please give how many tweets you want to get: "))
+print('Collecting stream tweets ...')
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+twitterStream = Stream(auth, listener())
+twitterStream.filter(track=["hillary"], languages=['en'])
+
+print(tweets)
+assert (len(tweets) == int(num))
