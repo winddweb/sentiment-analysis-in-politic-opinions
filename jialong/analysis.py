68c96d9f907be958558e44f125817b8e4a785c11
Jialong Li <jialong.li@citrix.com>
2016-04-27 21:29:46 -0700

    Add command line parameters to analysis.py

--- jialong/analysis.py
+++ jialong/analysis.py
@@ -1,9 +1,16 @@
 #! /usr/local/bin/python3
 
+import sys
 import training
 
+# ./analysis.py <start> <end> <step> <model>: ./analysis.py 100 5001 300 nb
+start = int(sys.argv[1])
+end = int(sys.argv[2])
+step = int(sys.argv[3])
+model = sys.argv[4]
+
 f = open('datafile','w')
-for num in range(100, 501, 100):
-    classifier, training_time, accuracy = training.do_training(num, 'nb')
+for num in range(start, end, step):
+    classifier, training_time, accuracy = training.do_training(num, model)
     f.write(str(num) + '   ' + str(training_time) + '   ' + str(accuracy) + '\n')
 f.close()
