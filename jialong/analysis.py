6c768f2f18599be2dd280bfa4cdbb690bd7604bd
Jialong Li <jialong.li@citrix.com>
2016-04-27 22:16:13 -0700

    Add mode parameter to analysis.py

--- jialong/analysis.py
+++ jialong/analysis.py
@@ -3,13 +3,14 @@
 import sys
 import training
 
-# ./analysis.py <start> <end> <step> <model>: ./analysis.py 100 5001 300 nb
+# ./analysis.py <start> <end> <step> <model> <mode>: ./analysis.py 100 5001 300 nb a
 start = int(sys.argv[1])
 end = int(sys.argv[2])
 step = int(sys.argv[3])
 model = sys.argv[4]
+mode = sys.argv[5] # 'w', 'a'
 
-f = open('datafile','w')
+f = open(model, mode) # model name as file name
 for num in range(start, end, step):
     classifier, training_time, accuracy = training.do_training(num, model)
     f.write(str(num) + '   ' + str(training_time) + '   ' + str(accuracy) + '\n')
